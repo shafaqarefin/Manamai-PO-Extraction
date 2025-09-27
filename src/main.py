d7f6e2e6b58fdea44ad61448d0a1598fca2117fd
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-09-28 02:44:02 +0600

    updated main function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-from camelot.io import read_pdf
-from utils.pdfPath import get_pdf_path
 from pathlib import Path
-from src.Extraction.text import extract_text
+from src.Excel.createExcel import save_excel_for_pdf
+from src.Extraction.pipeline import extract_PO_data
 
 
 def main():
@@ -9,8 +8,12 @@ def main():
     data_dir = base / "data"
 
     for file_path in data_dir.iterdir():
-        if file_path.is_file() and file_path.suffix.lower() == ".pdf":
-            print(extract_text(file_path))
+        if not file_path.is_file() or file_path.suffix.lower() != ".pdf":
+            continue
+
+        pdf_id = file_path.name.split('_')[0]
+        excel_data = extract_PO_data(str(file_path))
+        save_excel_for_pdf(pdf_id, excel_data)
 
 
 if __name__ == "__main__":
